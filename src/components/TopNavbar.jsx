11d8c14eead88ae7dfefc08ed5a7d3a1decaacdf
Ricky Kim <rkim@experientgroup.com>
2020-02-24 16:49:18 -0500

    Added CSS to style logo and sign in button within top navbar

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import whiteLogo from '../assets/images/skillpointlogo_white.png';
 
 export const TopNavbar = (props) => {
     return (
-        <div class="top-navbar">
+        <div className="top-navbar">
+            <img className="logo" src={whiteLogo} />
+            <div className="sign-in">Sign In</div>
         </div>
     )
 };
