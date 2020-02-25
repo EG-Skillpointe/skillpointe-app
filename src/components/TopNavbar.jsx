b32937772d62ab2585e12124d41c38cb7d2d9f20
Ricky Kim <rkim@experientgroup.com>
2020-02-25 10:12:17 -0500

    Added hamburger menu in top navbar

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import whiteLogo from '../assets/images/skillpointlogo_white.png';
+import hamburger from '../assets/images/hamburger_menu.png'
 
 export const TopNavbar = (props) => {
     return (
         <div className="top-navbar">
             <img className="logo" src={whiteLogo} />
             {/*<div className="sign-in">Sign In</div>*/}
+            <img className="hamburger" src={hamburger} />
         </div>
     )
 };
