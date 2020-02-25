377aef61c4f5ba191af6e9de4bd9f2c79eb8ecc8
Vivek Patel <vpatel@experientgroup.com>
2020-02-25 15:35:19 -0500

    added school data

--- src/components/TrainingCard.jsx
+++ src/components/TrainingCard.jsx
@@ -6,8 +6,8 @@ export const TrainingCard = (props) => {
         <img className="container__image" src="https://mtdata.ru/u7/photo53A0/20946911428-0/original.jpg#20946911428" alt="Cat"/>
           <div className="container__text">
             <h6 className="card-subtitle">School</h6>
-            <h5 className="card-title">Georgia Tech Institute of Nothing</h5>
-            <h6 className="card-text">Atlanta, GA</h6>
+            <h5 className="card-title">{props.school.name}</h5>
+            <h6 className="card-text">{props.school.location}</h6>
             <button className="card-button">Learn More</button>
           </div>
       </div>
