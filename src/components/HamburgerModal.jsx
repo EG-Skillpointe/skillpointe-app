346b3b097febcf76e11604e7e9514bd831e34d20
Viraj Shah <hello@virajashah.com>
2020-02-27 13:35:43 -0500

    Updated HamburgerModal text and removed footer link for Find People

--- src/components/HamburgerModal.jsx
+++ src/components/HamburgerModal.jsx
@@ -8,12 +8,12 @@ const HamburgerModal = (props) => {
             <ModalNavbar closeModal={props.closeModal}/>
 
             <div className="modal-body">
-                <Link className="" to="/" onClick={props.closeModal}>Main</Link>
+                <Link className="" to="/" onClick={props.closeModal}>Home</Link>
                 <Link className="" to="career-landing" onClick={props.closeModal}>Explore Careers</Link>
                 <Link className="" to="/jobsearch" onClick={props.closeModal}>Find Job</Link>
                 <Link className="" to="/training" onClick={props.closeModal}>Find Training</Link>
                 <Link className="" to="" onClick={props.closeModal}>Find People</Link>
-                <Link className="" to="" onClick={props.closeModal}>Additional Resrouces</Link>
+                <Link className="" to="" onClick={props.closeModal}>Additional Resources</Link>
                 <Link className="" to="" onClick={props.closeModal}>Contact</Link>
                 {/*<Link className="" to="/login">Login</Link>*/}
             </div>
