2f91be6d450450c545281105bc54f057b9c03df5
Illya Balakin <ibalakin@experientgroup.com>
2020-03-02 11:22:15 -0500

    play video inline and disable mobile-footer animation

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Video = (props) => {
   return (
     <div className='video-comp'>
-        <video controls preload="metadata" webkit-playsinline="true" playsinline="true">
+        <video controls preload="metadata" webkit-playsinline="true" playsInline={true}>
             <source src={`${props.video}#t=0.5`} type={props.type}/>
         </video>
     </div>
