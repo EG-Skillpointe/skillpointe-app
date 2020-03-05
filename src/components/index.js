92cd2d886b16d20a81cc654cc28acecaa50ae65c
Ricky Kim <rkim@experientgroup.com>
2020-03-05 10:53:37 -0500

    Removed TrainingCardV2

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,6 @@ export {default as SignUp} from './SignUp';
 export {default as MobileFooter} from './MobileFooter';
 export {default as AmbassadorCard} from './AmbassadorCard';
 export {default as TrainingCard} from './TrainingCard';
-export {default as TrainingCardV2} from './TrainingCardV2';
 export {default as JobsCard} from './JobsCard';
 export {default as Footer} from './Footer';
 export {default as TopNavbarBlue} from './TopNavbarBlue';
