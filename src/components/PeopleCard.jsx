654eb00203bdbc5868625fe3b408a44e65e9ba8e
Viraj Shah <hello@virajashah.com>
2020-03-05 16:07:25 -0500

    Updated people card styling

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
+import Button from "react-bootstrap/Button";
 
 export const PeopleCard = (props) => {
-    const image = require('../assets/images/profile_picture.png');
-
+    const image = require('../assets/images/illya.png');
+    
     return (
         <div className="people-card-containter">
             <div className="people-card-image-container">
                 <img className="people-card-image" src={image} alt="Tammy"/>
             </div>
             <div className="people-card-text-container">
-                    <h4 className="people-card-text">Tammy R</h4>
-                    <h6 className="people-card-text">Registerd Nurse</h6>
-                    <h6 className="people-card-text">1 mutual connection</h6>
+                    <h4 className="people-card-text">Illya Balakin</h4>
+                    <h6 className="people-card-text">Physical Therapist</h6>
+                    <h6 className="people-card-text">4 mutual connections</h6>
                     <div className="people-card-button-container">
-                        <div className="people-card-button-view">
-
-                        </div>
-                        <div className="people-card-button-view">
-
-                        </div>
-                    </div>
+                        <Button className="card-button" style={{margin:"2px 0", fontSize:"12px", height:"30px", textAlign:"center", lineHeight:"6px"}}>Connect</Button>
+                        <Button className="card-button" style={{margin:"2px 0", fontSize:"12px", height:"30px", textAlign:"center", lineHeight:"6px"}}>Remove</Button>                    </div>
             </div>
         </div>
   )
