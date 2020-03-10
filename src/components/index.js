972536559a15f3bf035cd20f6d10f3bee08f17a7
Viraj Shah <hello@virajashah.com>
2020-03-10 16:41:39 -0400

    Create FinanceCard component

--- src/components/index.js
+++ src/components/index.js
@@ -46,3 +46,4 @@ export {default as CompareCareers} from './CompareCareers';
 export {default as HamburgerModal} from './HamburgerModal';
 export {default as PeopleConnectCard} from './PeopleConnectCard';
 export {default as FeaturedJobs} from './FeaturedJobs';
+export {default as FinanceCard} from './FinanceCard';
