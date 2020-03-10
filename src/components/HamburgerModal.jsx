6f7c6866ed2714525f7c0b97463f32277d1c177c
Viraj Shah <hello@virajashah.com>
2020-03-10 14:41:17 -0400

    Fixed navigation and removed double tap to zoom

--- src/components/HamburgerModal.jsx
+++ src/components/HamburgerModal.jsx
@@ -55,7 +55,7 @@ const HamburgerModal = (props) => {
                 <Link style={careerLandingOrange} to="/career-landing" onClick={props.closeModal}>Explore Careers</Link>
                 <Link style={jobOrange} to="/jobsearch" onClick={props.closeModal}>Find Job</Link>
                 <Link style={trainingOrange} to="/training" onClick={props.closeModal}>Find Training</Link>
-                <Link style={peopleOrange} to="" onClick={props.closeModal}>Find People</Link>
+                <Link style={peopleOrange} to="/peoplesearch" onClick={props.closeModal}>Find People</Link>
                 <Link style={logout} to="/" onClick={() => {authService.logout(); props.closeModal();}}>Log Out</Link>
             </div>
 
