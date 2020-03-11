0c5dba04ad21612fedcb4e2329573c253b7e3dee
Vivek Patel <vpatel@experientgroup.com>
2020-03-11 14:14:56 -0400

    updated the career detail page to fix the bugs

--- src/components/WeldingSchools.jsx
+++ src/components/WeldingSchools.jsx
@@ -32,30 +32,30 @@ export const WeldingSchools = (props) => {
         )
     } else {
         return (
-            <div className="certification-container" style={{padding:'16px 20px 16px 20px', marginBottom:"10px"}}>
-                <h5 className="certification-header" style={{margin:"10px auto"}}>Welding Schools in you area</h5>
-                <div className="certification-item" style={{height:"50px", alignItems:"center"}}>
+            <div className="certification-container" style={{padding:'0px 14px 0px 14px', marginBottom:"10px"}}>
+                <h5 className="certification-header" style={{margin:"14px auto"}}>Welding Schools in Your Area</h5>
+                <div className="certification-item" style={{height:"58px", alignItems:"center"}}>
                     <div style={{display:"flex", flexDirection:"column", justifyContent:"center", width:"auto", height:"100%"}}>
                         <h6 className="certification-text" style={{paddingLeft:"15px", margin:"5px 0",  lineHeight:"100%", width:"85%", color:"#2D426B"}}><b>Northern Georgia Community College</b></h6>
                         <h6 className="certification-text" style={{paddingLeft:"15px", margin:"5px 0", fontSize:"12px", lineHeight:"100%", width:"auto"}}>Austell, GA</h6>
                     </div>
                     <img src={check} alt="" style={{width:"25px", position:"absolute", top:"40%", right:"20px"}}/>
                 </div>
-                <div className="certification-item" style={{height:"50px", alignItems:"center"}}>
+                <div className="certification-item" style={{height:"58px", alignItems:"center"}}>
                     <div style={{display:"flex", flexDirection:"column", justifyContent:"center", width:"auto", height:"100%"}}>
                         <h6 className="certification-text" style={{paddingLeft:"15px", margin:"5px 0",  lineHeight:"100%", width:"85%", color:"#2D426B"}}><b>Atlanta Technical College</b></h6>
                         <h6 className="certification-text" style={{paddingLeft:"15px", margin:"5px 0", fontSize:"12px", lineHeight:"100%", width:"auto"}}>Atlanta, GA</h6>
                     </div>
                     <img src={check} alt="" style={{width:"25px", position:"absolute", top:"40%", right:"20px"}}/>
                 </div>
-                <div className="certification-item" style={{height:"50px", alignItems:"center"}}>
+                <div className="certification-item" style={{height:"58px", alignItems:"center"}}>
                     <div style={{display:"flex", flexDirection:"column", justifyContent:"center", width:"auto", height:"100%"}}>
                         <h6 className="certification-text" style={{paddingLeft:"15px", margin:"5px 0",  lineHeight:"100%", width:"85%", color:"#2D426B"}}><b>Fortis College</b></h6>
                         <h6 className="certification-text" style={{paddingLeft:"15px", margin:"5px 0", fontSize:"12px", lineHeight:"100%", width:"auto"}}>Smyrna, GA</h6>
                     </div>
                     <img src={check} alt="" style={{width:"25px", position:"absolute", top:"40%", right:"20px"}}/>
                 </div>
-                <button style={{border:"none", color:"#FFFFFF", backgroundColor:"#F8A141", height:"22px", width:"80%", margin:"auto", fontSize:"12px", fontFamily:"Roboto", fontWeight:"bold", borderRadius:"5px", lineHeight:"14px"}}>More Atlanta Welding Schools</button>
+                <button style={{border:"none", color:"#FFFFFF", backgroundColor:"#F8A141", height:"25px", width:"70%", margin:"8px 46px 12px 46px", fontSize:"12px", fontFamily:"Roboto", fontWeight:"bold", borderRadius:"5px", lineHeight:"14px"}}>More Atlanta Welding Schools</button>
             </div>
         )
     }
