8aeb23d45b015f39f66df4065bf1a0b46acbdc20
Anthony Nguyen <anguyen@experientgroup.com>
2020-04-08 13:02:07 -0400

    video pausing on carousel scroll

--- src/components/VideoCarousel.js
+++ src/components/VideoCarousel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
 import {Video} from "./index";
@@ -8,6 +8,18 @@ import travisVideo from "../assets/videos/Travis_Edmonds_Ambassador Video.mp4";
 export  const VideoCarousel = props => {
   // const leftArrow = <button className="carousel-button "><span className="carousel-arrows carousel-arrow-left">prev</span></button>;
   // const rightArrow = <button className="carousel-button"><span className="carousel-arrows carousel-arrow-right">next</span></button>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = value => {
+    setValue(value);
+    var carouselItems = document.getElementsByClassName("BrainhubCarouselItem")
+    for(var i = 0; i < carouselItems.length; i++){
+      if(!carouselItems[i].classList.contains("BrainhubCarouselItem--active")){
+        // Pausing the video element
+        carouselItems[i].childNodes[0].childNodes[0].pause();
+      }
+    }
+  }
 
   return (
       <div className="App">
@@ -17,6 +29,8 @@ export  const VideoCarousel = props => {
                   slidesPerPage={1}
                   // addArrowClickHandler
                   autoPlay={5000}
+                  value={value}
+                  onChange={handleChange}
                   stopAutoPlayOnHover={true}
                   animationSpeed={1000}
                   infinite
@@ -25,7 +39,7 @@ export  const VideoCarousel = props => {
           <Video video={homepageVideo} type='video/mp4'/>
           <Video video={travisVideo} type='video/mp4'/>
           <Video video={homepageVideo} type='video/mp4'/>
-          <Video video={travisVideo} type='video/mp4'/>
+          <Video  video={travisVideo} type='video/mp4'/>
         </Carousel>
       </div>
   )
